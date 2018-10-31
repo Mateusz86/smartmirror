e8aba517b1e833c77a39380809a8b089bbb10093
unknown <you@example.com>
2018-10-31 22:32:46 +0100

    Add flow values in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,8 +29,6 @@ export class AppComponent {
 
 
   // na koncu poprzenosic
-
-
   constructor(private http: HttpClient, public datepipe: DatePipe) {
   }
 
@@ -65,35 +63,21 @@ export class AppComponent {
   private getData() {
 
     this.http.get('http://api.openweathermap.org/data/2.5/weather?q=krakow&APPID=abf5190fa4b7a6afd9c7d6ab2be3332f').subscribe(data => {
-        console.log(data);
-        // if(data.code==200) {
         this.weather = data;
-        // }
       }
     );
-
 
     this.date = new Date;
     this.formated_date = this.datepipe.transform(this.date, 'yyyy-MM-dd');
 
     this.http.get('http://mdrozdz6.webd.pl/mapa/wody/getData.php?st=1&dateFrom=' + this.formated_date).subscribe(data => {
-        // if(data.cod==200) {
+      console.log(data);
         this.water = data[0];
-        console.log(this.water);
-
-        // przeplywy
-        console.log(this.water.highValue);
-        console.log(this.water.lowValue);
-        console.log(this.water.item[this.water.item.length - 1].y);
-
-        // }
       }
     );
 
-
     // sprobowac zawezic nanneij danych 
     // nawykresie teraz ,za 6, godz za 12, 24,48(za 2 dni na 12)
-
 
     this.http.get('http://api.openweathermap.org/data/2.5/forecast?q=krakow&APPID=abf5190fa4b7a6afd9c7d6ab2be3332f').subscribe(data => {
       console.log(data);
@@ -103,10 +87,12 @@ export class AppComponent {
       // this.future_weather[i].dt_txt  -> wziac z tego godzine
       //this.datepipe.transform(this.future_weather[i].dt_txt, 'yyyy-MM-dd');
 
-      // array z tenparaturami
       this.future_weather.list.forEach(element => {
         this.temperatures.push(element.main.temp - 273);
         if (element.rain) {
+          
+
+          // return (n+moveY)*finalAmplitude/amplitude;
           this.rainfalls.push(element.rain['3h']);
         } else {
           this.rainfalls.push(0);
@@ -135,7 +121,6 @@ export class AppComponent {
           ],
           labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         },
-        // options: options
       });
     });
 
